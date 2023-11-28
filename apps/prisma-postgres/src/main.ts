09c4032b55f8311f0b36c588c54ac303647b73f5
Syd Amir <hemedani@gmail.com>
2023-11-28 20:35:00 +0330

    add cors

--- apps/prisma-postgres/src/main.ts
+++ apps/prisma-postgres/src/main.ts
@@ -2,26 +2,29 @@ import bodyParser from "body-parser";
 import express from "express";
 import { createPrismaConnection } from "./database/connection";
 import restRoutes from "./routes";
+import cors from "cors"
 
 try {
   const app = express();
-  
+
   const SERVER_PORT = process.env.SERVER_PORT || 9900;
-  
+
   (async () => {
     await createPrismaConnection();
   })
+
+  app.use(cors())
 
   restRoutes(app);
   app.use(express.json());
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-  
+
   app.listen(SERVER_PORT, () => {
     console.log(`Express server ( prisma + rest ) is up at http://localhost:${SERVER_PORT}`);
   });
-  
+
 } catch (error) {
   process.on('SIGINT', () => {
     console.info("exit process ...")
